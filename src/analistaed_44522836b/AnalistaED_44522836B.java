edbbe8c9c6b9a7313f080f1e6299c7a6ada9ecfb
AFRICA PONT <africapontlillo@gmail.com>
2021-01-13 19:51:46 +0100

    código sin javadoc DNI 44522836B

---
+++ src/analistaed_44522836b/AnalistaED_44522836B.java
@@ -0,0 +1,37 @@
+
+package analistaed_44522836b;
+import java.util.*;
+
+public class AnalistaED_44522836B {
+
+    
+  static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public AnalistaED_44522836B(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA        
+        AnalistaED_44522836B emp = new AnalistaED_44522836B("AFRICA PONT", "44522836B");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto 
+		+ " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+                + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+
+}
+    
+
